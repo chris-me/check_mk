9ff960a71f16cbc291482031faa76ae51061e1ee
Christian Metz <christianm@mailbox.org>
2017-07-18 09:06:02 +0200

    plugins/ms_exch_db_whitespace - bash install script

--- plugins/msexch_db_whitespace/install.sh
+++ plugins/msexch_db_whitespace/install.sh
@@ -2,8 +2,8 @@
 
 # Installation script for the plugin (mkp installation routine to be done)
 
-echo "!!! This script assumes the installation of CheckMK is in"
-echo "/opt/omd/sites/USERNAME"
+echo "!!! This script assumes the installation of CheckMK is in  !!!"
+echo "!!! /opt/omd/sites/USERNAME                                !!!"
 
 SCRIPT_PATH=`dirname "$0"`
 SCRIPT_PATH=`( cd "$SCRIPT_PATH" && pwd )`
@@ -15,12 +15,14 @@ if [ "$ret" -ne 0 ];then
   echo "Error - the specified user does not exist!"
   exit 1
 fi
-
 SITE_PATH="/opt/omd/sites/${USERNAME}"
 if [ ! -d "$SITE_PATH" ]; then
   echo "Error - the specified directory does not exist!"
   exit 1
 fi
 
-cp -v $SCRIPT_PATH/server/checks/* $SITE_PATH/local/share/check_mk/checks/
+cp -v $SCRIPT_PATH/server/checks/msexch_db_whitespace $SITE_PATH/local/share/check_mk/checks/
+chown $USERNAME:$USERNAME $SITE_PATH/local/share/check_mk/checks/msexch_db_whitespace
 
+cp -v $SCRIPT_PATH/server/wato/msexch_db_whitespace.py $SITE_PATH/local/share/check_mk/web/plugins/wato/
+chown $USERNAME:$USERNAME $SITE_PATH/local/share/check_mk/web/plugins/wato/msexch_db_whitespace.py
